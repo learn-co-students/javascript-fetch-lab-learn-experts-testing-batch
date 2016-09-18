1ff3f47e172b55e99eeea4ea8a0ddcaf54096710
Matthew <pickledyamsman@gmail.com>
2016-09-18 15:27:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 function getIssues() {
+  fetch(`https:/api.github.com/repos/pickledyamsman/javascript-fetch-lab/issues`).
+    then(resp => resp.json()).
+    then(json => showIssues(json))
 }
 
 function showIssues(json) {
+  const template = Handlebars.compile(document.getElementById('issues-template').innerHTML)
+  document.getElementById('issues').innerHTML = template(json)
 }
 
 function createIssue() {
+  fetch(`https://api.github.com/repos/pickledyamsman/javascript-fetch-lab/issues`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify({ title: document.getElementById('title').value, body: document.getElementById('body').value }) 
+  }).then(resp => getIssues())
 }
 
 function showResults(json) {
+  const template = Handlebars.compile(document.getElementById('repo-template').innerHTML)
+  document.getElementById('results').innerHTML = template(json)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(resp => resp.json()).then(json => showResults(json));
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
