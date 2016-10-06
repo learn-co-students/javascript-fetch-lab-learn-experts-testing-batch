b1aa7a84fe492f72f92d31917753103fca59b6bb
pajamaw <pjpeterww@gmail.com>
2016-10-06 11:36:03 -0400

    Possibly consider talking a little bit more about handlebars as I wouldn't assume that students would understand that they'd have previous knowledge of it or even understand that they have to change the html

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
+const user = 'pajamaw'
+const base = 'https://api.github.com/'
+const fork = `${user}/javascript-fetch-lab`
+const repo = 'learn-co-curriculum/javascript-fetch-lab'
+
 function getIssues() {
+
+  fetch(`${base}repos/${fork}/issues`)
+    .then(resp => resp.json())
+    .then(json => showIssues(json))
 }
 
 function showIssues(json) {
+  const template = Handlebars.compile(document.getElementById('issues-template').innerHTML)
+  document.getElementById('issues').innerHTML = template(json)
 }
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const issueData = { title: issueTitle, body: issueBody }
+
+  fetch(`${base}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(issueData)
+  }).then(resp => getIssues())
 }
 
 function showResults(json) {
+  const template = Handlebars.compile(document.getElementById('repo-template').innerHTML)
+  document.getElementById('results').innerHTML = template(json)
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
+  fetch(`${base}repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(resp => resp.json())
+    .then(json => showResults(json))
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
+
   return ''
 }
