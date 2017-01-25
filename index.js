1a83e8e1084cc3123da0a13bea1d8802fc3ca2ba
jd2rogers2 <jd2rogers2@gmail.com>
2017-01-25 13:33:18 -0500

    switching to IDE

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ function createIssue() {
 }
 
 function showResults(json) {
+  debugger;
+  document.querySelector('#results').append(json)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch('https://api.github.com/repos/' + repo)
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return 'd8628f19a8534cba9f9df5c903ca926194d79590'
 }
