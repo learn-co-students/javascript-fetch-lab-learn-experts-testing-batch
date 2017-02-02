193338f2d20baea80a5844d510cd04b54123c61a
mendelB <mendelblesofsky@gmail.com>
2017-02-01 23:46:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
+const myRepo = 'mendelB/javascript-fetch-lab'
+const base = 'https://api.github.com/repos/'
+const issueUrl = base + myRepo + '/issues'
+
 function getIssues() {
+	fetch(issueUrl, {
+  		headers: {
+    		Authorization: `token ${token}`
+  		}
+	}).then(res => res.json()).then(json => showIssues(json))
 }
 
 function showIssues(json) {
+	let html = document.getElementById("issues-template").innerHTML;
+	var template = Handlebars.compile(html);
+	document.getElementById("results").innerHTML += template(json)
 }
 
 function createIssue() {
+	const title = document.getElementById('title').value;
+	const body = document.getElementById('body').value;
+	fetch(issueUrl, {
+	  	method: 'post',
+	  	headers: {
+	  		Authorization: `token ${getToken()}`
+	  	},
+	  	body: JSON.stringify({
+	  		title: title,
+	  		body: body
+	  	})
+  	}).then(() => getIssues());
 }
 
 function showResults(json) {
+	var template = Handlebars.compile(document.getElementById("repo-template").innerHTML);
+	document.getElementById("results").innerHTML = template(json);
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  fetch(base + repo +'/forks', {
+  	method: 'post',
+  	headers: {
+  		Authorization: `token ${getToken()}`
+  	}
+  }).then(res => res.json()).then(json => showResults(json));
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
